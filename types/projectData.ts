bcfd0f853d3b76fc890d493ed7f7f77b6e693141
ALVIN-DESKTOP\Alvin Toh <alvin4292@outlook.com>
2024-03-17 02:29:36 +0800

    feat(project): remove redundant fields from projectData to improve loading speed

--- types/projectData.ts
+++ types/projectData.ts
@@ -4,33 +4,8 @@ interface ProjectData {
   link: string;
   githubLink: string;
   iconName: string;
-  image: Image;
-  body: Body;
   _id: string;
-  _raw: Raw;
-  type: string;
   slug: string;
-}
-
-interface Image {
-  height: number;
-  width: number;
-  url: string;
-  type: string;
-  _raw: Record<string, unknown>;
-}
-
-interface Body {
-  raw: string;
-  code: string;
-}
-
-interface Raw {
-  sourceFilePath: string;
-  sourceFileName: string;
-  sourceFileDir: string;
-  contentType: string;
-  flattenedPath: string;
 }
 
 export type { ProjectData };
